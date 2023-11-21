471781770ba4b2f2c11e51a5bec1719ea3a4e1b5
ruveyda <rtyty@MacBook-Air.local>
2023-11-21 16:21:16 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Product from "./pages/Product"
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <div className="">
+      <Router>
+        <Header />
+        <Routes>
+          <Route index path="/" element={<Product />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
